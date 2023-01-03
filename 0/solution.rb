b4ce503f7ad41b61c24ae5ac78937a667150d74c
Caitlin Palmer-Bright <catlinadonaldson@gmail.com>
2023-01-03 16:21:55 +1100

    add empty template

---
+++ 0/solution.rb
@@ -0,0 +1,13 @@
+require 'byebug'
+require 'active_support'
+require 'active_support/core_ext'
+
+sample = true
+# sample = false
+
+lines = File.read(sample ? 'sample.txt' : 'input.txt').split("\n").map do |line|
+  line.strip.split(' ')
+end
+
+puts "part one: #{''}"
+puts "part two: #{''}"
