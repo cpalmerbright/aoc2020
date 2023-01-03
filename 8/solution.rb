05c4e93c3bcf2a5b2de7961607dbc9b35f168489
cpalmerbright <caitlinadonaldson@gmail.com>
2023-01-03 16:19:03 +1100

    ignore byebug files, finish day 8

--- 8/solution.rb
+++ 8/solution.rb
@@ -2,13 +2,63 @@ require 'byebug'
 require 'active_support'
 require 'active_support/core_ext'
 
-sample = true
-# sample = false
+# sample = true
+sample = false
 
 lines = File.read(sample ? 'sample.txt' : 'input.txt').split("\n").map do |line|
-  line.strip
+  line.strip.split(' ')
 end
 
-puts "part one: #{'part_one'}"
-puts "part two: #{'part_two'}"
+acc = 0
+lines_visited = []
+next_line = 0
+
+while !lines_visited.include?(next_line)
+  line = lines[next_line]
+  lines_visited << next_line
+  val = line[1].to_i
+  if line[0] == 'nop'
+    next_line += 1
+  elsif line[0] == 'acc'
+    acc += val
+    next_line += 1
+  elsif line[0] == 'jmp'
+    next_line += val
+  end
+end
+puts "part one: #{acc}"
+
+lines.length.times do |line_to_check|
+  next if next_line >= lines.length
+  acc = 0
+  lines_visited = []
+  next_line = 0
+
+  while next_line < lines.length && !lines_visited.include?(next_line)
+    line = lines[next_line]
+    lines_visited << next_line
+    val = line[1].to_i
+    if next_line == line_to_check
+      if line[0] == 'jmp'
+        next_line += 1
+      elsif line[0] == 'acc'
+        acc += val
+        next_line += 1
+      elsif line[0] == 'nop'
+        next_line += val
+      end
+    else
+      if line[0] == 'nop'
+        next_line += 1
+      elsif line[0] == 'acc'
+        acc += val
+        next_line += 1
+      elsif line[0] == 'jmp'
+        next_line += val
+      end
+    end
+  end
+end
+
+puts "part two: #{acc}"
 
